d8f7cc4b660904842e5246481f00f27d6b2d88a4
Brian Almendras <brian.almendras@ucb.edu.bo>
2025-03-19 21:50:57 -0400

    Select active link and remember its state

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,9 +1,14 @@
-import Link from "next/link";
+'use client' 
 
-export const metadata={
+import Link from "next/link";
+import {usePathname} from "next/navigation"
+import "./../styles.css"
+
+const metadata={
     title: "With auth layout",
     description: "this for the practice"
-}
+} 
+
 const navLinks=[
     {name: "Register",href: "/register"},
     {name: "Login",href: "/login"},
@@ -14,12 +19,16 @@ export  default function AuthLayout({
 }:{
     children: React.ReactNode;
 }) {
+    const pathname=usePathname();
+    
     return(
         <div>
             <h2> Inner layout</h2>
             {navLinks.map((link)=>{
+                const isActive=pathname.startsWith(link.href)
                 return (
-                    <Link href={link.href} key={link.name}>
+                    <Link href={link.href} key={link.name}
+                    className={isActive? "font-bold mr-4": "text-green-500 mr-4"}>
                         {link.name}
                     </Link>
                 )
